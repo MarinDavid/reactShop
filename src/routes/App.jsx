e3e6e71e70580e665bb6ad2b29716e58c02e8fa1
David Marín <marindavid1399@gmail.com>
2022-04-05 21:41:28 +0200

    Proyecto antes de introducir un gran número de componentes

---
+++ src/routes/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
+import '../styles/global.css';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element = {Home()} />
+                    <Route path="/login" element = {Login()} />
+                    <Route path="/recovery-password" element = {RecoveryPassword()} />
+                    <Route path="/send-email" element = {SendEmail()} />
+                    <Route path="/new-password" element = {NewPassword()} />
+                    <Route path="/account" element = {MyAccount()} />
+                    <Route path="/signup" element = {CreateAccount()} />
+                    <Route path="/checkout" element = {Checkout()} />
+                    <Route path="/orders" element = {Orders()} />
+                    <Route path="*" element = {NotFound()} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
+}
+
+export default App;
